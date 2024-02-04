59a801ad1a625fa3ac08c29e74aa2e409bb33157
catwithtudou <949812478@qq.com>
2024-02-04 09:59:56 +0800

    update: default by script

---
+++ push.sh
@@ -0,0 +1,37 @@
+echo "Ready to execute script"
+
+# 默认 git commit message
+msg="update: default by script"
+
+# 默认 commit 分之
+branch="main"
+
+if [  $1 ]
+then
+  msg=$1
+fi
+
+if [  $2 ]
+then
+  branch=$2
+fi
+
+echo "git commit message:[$msg]"
+echo "git branch:[$branch]"
+
+sleep 1
+
+# 执行 git push 过程
+git add .
+git status
+
+sleep 1
+
+git commit -m "$msg"
+
+sleep 1
+
+# git push origin "$branch"
+
+# sleep 1
+
